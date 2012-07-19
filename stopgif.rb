46b02f26a7d5751cf8543477f6418ffce93a7b5d
swdyh <youhei@gmail.com>
2012-07-19 15:34:56 +0900

    added example

--- stopgif.rb
+++ stopgif.rb
@@ -39,10 +39,22 @@ get '/' do
 
           /?url=http%3A%2F%2Fexample.com%2Ffoo.gif
 
-     ## options
+      ## options
 
           /?url=http%3A%2F%2Fexample.com%2Ffoo.gif&index=5
           /?url=http%3A%2F%2Fexample.com%2Ffoo.gif&alt=http%3A%2F%2Fexample.com%2Fbar.gif
+
+      ## example
+
+      animation gif url: http://upload.wikimedia.org/wikipedia/commons/6/6a/Sorting_quicksort_anim.gif
+
+          http://stopgif.herokuapp.com/?url=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F6a%2FSorting_quicksort_anim.gif
+
+      specify scene index.
+
+          http://stopgif.herokuapp.com/?url=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F6a%2FSorting_quicksort_anim.gif&index=20
+
+      ## source
 
       https://github.com/swdyh/stopgif
 
